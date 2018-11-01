0203b5ce77864affb15ee4993e2a726ac3e61884
TheSolly <salaheldin@gmail.com>
2018-11-01 17:04:45 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+import Form from "./Form";
+import "./App.css";
+
+export default () => {
+  const [todos, setTodos] = useState([]);
+  const toggleComplete = i =>
+    setTodos(
+      todos.map(
+        (todo, k) => (k === i ? { ...todo, complete: !todo.complete } : todo)
+      )
     );
-  }
-}
 
-export default App;
+  return (
+    <div className="App">
+      <Form
+        onSubmit={text => setTodos([...todos, { text, complete: false }])}
+      />
+      <div>
+        {todos.map(({ text, complete }, i) => (
+          <div
+            style={{
+              textDecoration: complete ? "line-through" : ""
+            }}
+            key={text}
+            onClick={() => toggleComplete(i)}
+          >
+            {text}
+          </div>
+        ))}
+      </div>
+      <button onClick={() => setTodos([])}>Reset</button>
+    </div>
+  );
+};
